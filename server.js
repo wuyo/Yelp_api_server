f33af5dcd5846ec29b556561b2c2a90d991e55ec
wuyo <wuyo@oregonstate.edu>
2019-06-13 17:25:34 -0700

    Finished

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const redis = require('redis');
+
+const redisPort = process.env.REDIS_PORT || 6379;
+const redisHost = process.env.REDIS_HOST;
+const redisClient = redis.createClient(redisPort, redisHost);
 
 const api = require('./api');
 
 const { connectToDB } = require('./lib/mongo');
-// const { connectToRabbitMQ, getChannel } = require('./lib/rabbitmq');
-
+const rateLimitWindowMillis = 60000;
+const rateLimitWindowMaxRequests = 5;
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -34,6 +39,69 @@ app.use('*', function (req, res, next) {
 // app.listen(port, function() {
 //   console.log("== Server is running on port", port);
 // });
+
+function getTokenBucket(ip) {
+  return new Promise((resolve, reject) => {
+    redisClient.hgetall(ip, (err, tokenBucket) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (tokenBucket) {
+          tokenBucket.tokens = parseFloat(tokenBucket.tokens);
+        } else {
+          tokenBucket = {
+            tokens: rateLimitWindowMaxRequests,
+            last: Date.now()
+          };
+        }
+        resolve(tokenBucket);
+      }
+    });
+  });
+}
+
+function saveTokenBucket(ip, tokenBucket) {
+  return new Promise((resolve, reject) => {
+    redisClient.hmset(ip, tokenBucket, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function rateLimit(req, res, next) {
+  try {
+    const tokenBucket = await getTokenBucket(req.ip);
+
+    const timestamp = Date.now();
+    const ellapsedMillis = timestamp - tokenBucket.last;
+    const refreshRate = rateLimitWindowMaxRequests / rateLimitWindowMillis;
+    tokenBucket.tokens += refreshRate * ellapsedMillis;
+    tokenBucket.tokens = Math.min(rateLimitWindowMaxRequests, tokenBucket.tokens);
+    tokenBucket.last = timestamp;
+
+    if (tokenBucket.tokens >= 1) {
+      tokenBucket.tokens -= 1;
+      saveTokenBucket(req.ip, tokenBucket);
+      next();
+    } else {
+      saveTokenBucket(req.ip, tokenBucket);
+      res.status(429).send({
+        error: "Too many requests per minute"
+      });
+    }
+
+  } catch (err) {
+    console.error(err);
+    next();
+  }
+}
+
+app.use(rateLimit);
+
 
 connectToDB(async () => {
   // await connectToRabbitMQ('csv');
